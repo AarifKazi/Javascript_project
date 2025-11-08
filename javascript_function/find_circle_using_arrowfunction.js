94de851b2226609a277f1d62e0246e52a81ab295
AarifKazi <Kaziaarif61@gmail.com>
2025-11-08 17:23:46 +0530

    Delete javascript_function directory

--- javascript_function/find_circle_using_arrowfunction.js
+++
@@ -1,9 +0,0 @@
-const pi = 3.14;
-const r = Number(prompt('Enter Radius for find out circle'));
-
-const circle = (r) =>{
-       circle_area = pi*r*r;
-       console.log(`Area of circle is ${circle_area}`); 
-}
-
-circle(r);
--- javascript_function/square_function_using_arrowfunction.js
+++
@@ -1,8 +0,0 @@
-const a = Number(prompt('Enter Number'));
-
-const square = (a) =>{
-     let s = a * a;
-     console.log(`Square is ${s}`);
-}
-
-square(a);
